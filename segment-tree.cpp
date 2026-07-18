9f0b7e1689f8d856f3b15d0a2a24b70ac7d10390
pranay-gottimukula <pranaygottimukula@gmail.com>
2026-07-18 20:41:52 +0530

    lazy updation

--- segment-tree.cpp
+++ segment-tree.cpp
@@ -4,9 +4,11 @@ using namespace std;
 class segtree{
 public:
     vector<int> seg;
+    vector<int> lazy;
 
     segtree(int n){
         seg.resize(4 * n);
+        lazy.resize(4 * n);
     }
 
     void build(int ind, int low, int high, vector<int>& arr){
@@ -22,7 +24,24 @@ public:
         seg[ind] = max(seg[2 * ind + 1], seg[2 * ind + 2]);
     }
 
+    // To fix lazy updation
+    void push(int ind, int low, int high){
+        if(lazy[ind] != 0){
+            seg[ind] += lazy[ind];
+
+            if(low != high){
+                lazy[2 * ind + 1] += lazy[ind];
+                lazy[2 * ind + 2] += lazy[ind];
+            }
+
+            lazy[ind] = 0;
+        }
+    }
+
     int query(int ind, int low, int high, int l, int r){
+
+        push(ind, low, high);   // For Lazy updation
+    
         if(l <= low && high <= r){
             return seg[ind];
         }
@@ -37,7 +56,11 @@ public:
         return max(left, right);
     }
 
+    // Point update, updates value at index i to val
     void update(int ind, int low, int high, int i, int val){
+
+        push(ind, low, high);
+
         if(low == high){
             seg[ind] = val;
             return;
@@ -51,6 +74,33 @@ public:
         else{
             update(2* ind + 2, mid + 1, high, i, val);
         }
+
+        seg[ind] = max(seg[2 * ind + 1], seg[2 * ind + 2]);
+    }
+
+    // range addition, for i in range l to r, it updates arr[i] = arr[i] + val
+    void updatelazy(int ind, int low, int high, int l, int r, int val){
+
+        push(ind, low, high);
+
+        if(r < low || l > high){
+            return;
+        }
+
+        if(l <= low && high <= r){
+            seg[ind] += val;
+
+            if(low != high){
+                lazy[2 * ind + 1] += val;
+                lazy[2 * ind + 2] += val;
+            }
+
+            return;
+        }
+
+        int mid = low + (high - low) / 2;
+        updatelazy(2 * ind + 1, low, mid, l, r, val);
+        updatelazy(2 * ind + 2, mid + 1, high, l, r, val);
 
         seg[ind] = max(seg[2 * ind + 1], seg[2 * ind + 2]);
     }
